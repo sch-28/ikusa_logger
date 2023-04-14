79bd2152a74913c42884cc1aeab1caa096750be0
sch-28 <jansch2803@googlemail.com>
2023-04-14 23:18:06 +0200

    feat: status check

--- logger/src/options/status_check.py
+++ logger/src/options/status_check.py
@@ -1,11 +1,27 @@
 import os
+from datetime import datetime, timedelta
 from .. import config
+
+
+def is_outdated():
+    date = datetime.strptime(config.config.patch, "%d.%m.%Y")
+    now = datetime.now()
+    delta = now - date
+    return delta > timedelta(days=7)
+
 
 def check_health():
     if os.path.exists(os.path.join(os.environ['SystemRoot'], 'System32', 'drivers', 'npcap.sys')):
-        print("npcap is installed")
+        print("Npcap is installed", flush=True)
     else:
-        print("npcap is not installed")
-        
+        print("Npcap is not installed", flush=True)
+
     if config.config.invalid:
-        print("config file is invalid")
+        print("Could not locate config file or config is invalid", flush=True)
+        return
+
+    
+    if is_outdated():
+        print("The config is older than 7 days. It might not work anymore. Try to update the config by using:\nlogger.exe -u", flush=True)
+    else:
+        print("The config is up to date.", flush=True)
