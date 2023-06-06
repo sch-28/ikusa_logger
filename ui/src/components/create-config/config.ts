df1961a4ff42c839034092cd80c1c8bcada48955
sch-28 <jansch2803@googlemail.com>
2023-06-06 23:50:36 +0200

    feat: config parser

--- ui/src/components/create-config/config.ts
+++ ui/src/components/create-config/config.ts
@@ -1,4 +1,6 @@
 import { filesystem } from '@neutralinojs/lib';
+import ConfigIniParser from 'config-ini-parser';
+const config_parser = new ConfigIniParser.ConfigIniParser();
 
 export type Config = {
 	patch?: string;
@@ -46,6 +48,20 @@ player_two = ${config.player_two}
 kill = ${config.kill}
 log_length = 600
 name_length = 64`;
+}
+
+export async function get_config() {
+	const raw_config = await filesystem.readFile('config.ini');
+	const parsed_config = config_parser.parse(raw_config);
+	const config: Config = {
+		identifier: parsed_config.get('PACKAGE', 'identifier'),
+		player_one: parsed_config.get('PACKAGE', 'player_one'),
+		player_two: parsed_config.get('PACKAGE', 'player_two'),
+		guild: parsed_config.get('PACKAGE', 'guild'),
+		kill: parsed_config.get('PACKAGE', 'kill')
+	};
+	return config;
+
 }
 
 export async function update_config(config: Config) {
