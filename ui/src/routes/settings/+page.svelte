6adf5e91b43fe52de2be2f9609ac41eee91b6483
sch-28 <jansch2803@googlemail.com>
2023-07-27 16:12:45 +0200

    feat: restart as dev button

--- ui/src/routes/settings/+page.svelte
+++ ui/src/routes/settings/+page.svelte
@@ -3,6 +3,9 @@
 	import Select from '../../components/create-config/select.svelte';
 	import { onMount } from 'svelte';
 	import { get_config, type Config, update_config } from '../../components/create-config/config';
+	import Button from '../../svelte-ui/elements/button.svelte';
+	import { app, os } from '@neutralinojs/lib';
+	import { dev } from '$app/environment';
 
 	let config: Config;
 
@@ -19,13 +22,23 @@
 		selected_interface =
 			config.all_interfaces === true || config.all_interfaces === undefined ? 0 : 1;
 	});
+
+	async function restart_dev() {
+		if (dev) return;
+		await os.execCommand(`ikusa-logger-win_x64.exe --window-enable-inspector`, {
+			background: true
+		});
+		app.exit();
+	}
 </script>
 
-<div>
+<div class="h-full flex flex-col items-center">
 	<Label>Network Interface</Label>
 	<Select
 		options={['All', 'Default']}
 		bind:selected_value={selected_interface}
 		on_change={update_interface}
 	/>
+
+	<Button class="mt-auto" on:click={restart_dev}>Restart as dev</Button>
 </div>
