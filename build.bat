25b9a623e54fd26eed38298cdcdb227e5eaaa052
sch-28 <jansch2803@googlemail.com>
2025-03-12 21:07:13 +0100

    chore: prepare project reanimation

--- build.bat
+++ build.bat
@@ -12,7 +12,6 @@ xcopy update.bat dist\ikusa-logger\ /Y
 :: Build the Frontend
 cd ui 
 CALL npm i
-CALL npm run build
 
 :: Compile the program
 cd .. 
@@ -20,10 +19,10 @@ CALL neu update
 CALL neu build
 
 :: Timeout for 2 seconds to make sure the resource is free
-timeout 2 /nobreak
+::timeout 2 /nobreak
 
 :: Insert Manifest
-mt.exe -manifest ikusa.manifest -outputresource:dist\ikusa-logger\ikusa-logger-win_x64.exe;1
+::mt.exe -manifest ikusa.manifest -outputresource:dist\ikusa-logger\ikusa-logger-win_x64.exe;1
 
 :: Compile the installer
-ISCC build-setup.iss
+::ISCC build-setup.iss
